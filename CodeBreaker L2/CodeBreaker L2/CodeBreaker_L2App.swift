fe3240b09c2305dbf98bd493239e1335c06a558f
BestKora <developer@bestkora.com>
2026-02-11 18:45:47 +0300

    Code for Lecture 2

---
+++ CodeBreaker L2/CodeBreaker L2/CodeBreaker_L2App.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreaker_L2App.swift
+//  CodeBreaker L2
+//
+//  Created by Tatiana Kornilova on 11/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreaker_L2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
