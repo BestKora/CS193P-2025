58a73105f5e77a238320a6c67749ca581d244a40
BestKora <developer@bestkora.com>
2026-02-24 11:43:13 +0300

    Beginning of L3

---
+++ CodeBreaker L3/CodeBreaker L3/CodeBreaker_L3App.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreaker_L3App.swift
+//  CodeBreaker L3
+//
+//  Created by Tatiana Kornilova on 24/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreaker_L3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
