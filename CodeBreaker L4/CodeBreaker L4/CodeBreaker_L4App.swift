3f2e5afdffd0b90f279fae88003828f01e5be880
BestKora <developer@bestkora.com>
2026-03-04 19:45:46 +0300

    Lecture 4

---
+++ CodeBreaker L4/CodeBreaker L4/CodeBreaker_L4App.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreaker_L4App.swift
+//  CodeBreaker L4
+//
+//  Created by Tatiana Kornilova on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreaker_L4App: App {
+    var body: some Scene {
+        WindowGroup {
+            CodeBreakerView()
+        }
+    }
+}
