2905c99c5e6ea7d39d1836ea867952ee9e60f811
BestKora <developer@bestkora.com>
2026-03-22 15:02:19 +0300

    L6

---
+++ CodeBreaker L6/CodeBreaker L6/CodeBreaker_L6App.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreaker_L6App.swift
+//  CodeBreaker L6
+//
+//  Created by Tatiana Kornilova on 22/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreaker_L6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
