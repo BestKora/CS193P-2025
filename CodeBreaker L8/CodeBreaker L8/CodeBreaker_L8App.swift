ca78a070716c9b22873797d825844766e439a4be
BestKora <developer@bestkora.com>
2026-04-06 16:48:45 +0300

    L8

---
+++ CodeBreaker L8/CodeBreaker L8/CodeBreaker_L8App.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreaker_L8App.swift
+//  CodeBreaker L8
+//
+//  Created by Tatiana Kornilova on 06/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreaker_L8App: App {
+    var body: some Scene {
+        WindowGroup {
+            CodeBreakerView()
+        }
+    }
+}
