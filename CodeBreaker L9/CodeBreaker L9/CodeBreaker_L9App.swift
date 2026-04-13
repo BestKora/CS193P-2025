609be31bd27dd98e85c6970e4e7e60c534e13e2b
BestKora <developer@bestkora.com>
2026-04-13 09:50:54 +0300

    L9

---
+++ CodeBreaker L9/CodeBreaker L9/CodeBreaker_L9App.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreaker_L9App.swift
+//  CodeBreaker L9
+//
+//  Created by Tatiana Kornilova on 13/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreaker_L9App: App {
+    var body: some Scene {
+        WindowGroup {
+            CodeBreakerView()
+        }
+    }
+}
