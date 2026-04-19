99bc30cd17ed275d55ffd6723c0f3e9219134407
BestKora <developer@bestkora.com>
2026-04-19 13:52:08 +0300

    L 10

---
+++ CodeBreaker L10/CodeBreaker L10/CodeBreaker_L10App.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreaker_L10App.swift
+//  CodeBreaker L10
+//
+//  Created by Tatiana Kornilova on 19/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreaker_L10App: App {
+    var body: some Scene {
+        WindowGroup {
+            GameChooser()
+        }
+    }
+}
